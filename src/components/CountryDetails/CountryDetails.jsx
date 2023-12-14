da8e3c706b73f10edc0932d3497cd2bc8fd5543d
Nush <dasuneab4@gmail.com>
2023-12-14 22:54:10 +0530

    Initial project setup

---
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryDetails = () => {
+    return (
+        <div>
+            CD
+        </div>
+    );
+}
+
+export default CountryDetails;
---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchInput = () => {
+    return (
+        <div>
+            SI
+        </div>
+    );
+}
+
+export default SearchInput;
