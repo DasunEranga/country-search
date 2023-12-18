39071e65fa9d49c5b36b64eb9c90b074416da143
Dasun Bandara <dasun@eight25media.com>
2023-12-18 08:38:53 +0530

    cache handle using local storage

--- src/services/apiCountries.js
+++ src/services/apiCountries.js
@@ -1,14 +1,21 @@
 const fetchInitCountryListing = async (filter = '') => {   
     try {
-        const response = await fetch(`https://restcountries.com/v3.1/all?${filter}`);
-        const data = await response.json();
-        
-        if (!response.ok) {
-            // throw new Error('Network response was not ok');
-            console.error('Network response was not ok', response.statusText);
+        const cachedData = localStorage.getItem('cachedData');
+        if (cachedData) {
+            // setCountry(JSON.parse(cachedData));
+            return JSON.parse(cachedData);
+        } else {
+            const response = await fetch(`https://restcountries.com/v3.1/all?${filter}`);
+            const data = await response.json();
+            localStorage.setItem('cachedData', JSON.stringify(data));
+            if (!response.ok) {
+                // throw new Error('Network response was not ok');
+                console.error('All countries network response was not ok', response.statusText);
+            }
+            return data;
         }
+        
     
-        return data;
     } catch (error) {
         console.error('Error fetching country data:', error);
         throw error; // Rethrow the error to handle it in the calling code
@@ -22,7 +29,7 @@ const fetchSearchCountry = async (query) => {
         
         if (!response.ok) {
             // throw new Error('Network response was not ok');
-            console.error('Network response was not ok', response.statusText);
+            console.error('Filterd country network response was not ok', response.statusText);
             return [];
         }
         
